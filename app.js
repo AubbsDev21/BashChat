bec73b6c7171261db3ba3c7d974011957035c5eb
Aubre <bodyaubre@gmail.com>
2018-10-10 14:22:53 -0400

    Bug fixes and finshing up app

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const Chatkit = require("@pusher/chatkit-server");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Chatkit = require('@pusher/chatkit-server');
 
 const app = express();
 
 const chatkit = new Chatkit.default({
-  instanceLocator: "v1:us1:6f6edbd2-7a10-46b5-bea0-0fee822fc4dc",
-  key: "deff98a5-224f-4ebc-9a30-d68a4aa5e9ed:RzT30QeqrN3fBtF10gnQ4By/l52UDt4HRjBex9aWQjo="
+  instanceLocator: "v1:us1:f40ef635-3256-40f0-9124-7a554253bf3d",
+  key: "571e16ef-d0ac-40f3-b142-1c903edc763a:oszOyIj2O9q+TmftMRXDF+URfC+sTqioEnM+eCYqPqQ="
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,12 +34,15 @@ app.post("/users", (req, res) => {
       }
     });
 });
-//this function auth the chatkit users 
+//this function auth end point the chatkit users 
 app.post("/authenticate", (req, res) => {
   const authData = chatkit.authenticate({ userId: req.query.user_id });
   res.status(authData.status).send(authData.body);
 });
-//thio handles the apps server ports
+
+
+
+//this handles the apps server ports
 const port = 3001;
 app.listen(port, err => {
   if (err) {
--- client.js
+++ client.js
@@ -1,28 +1,101 @@
-const prompt = require('prompt')
+const axios = require('axios')
 const util = require('util')
-try {
-    const main = async () => {
-        prompt.start();
-       prompt.message = " "
-        const get = util.promisify(prompt.get)
-    
-        const usernameSchema =  [
-            {
-            description: 'Enter your username',
-            name: 'username',
-            required: true
-    
-        }
-    ]
-    
-    const { username} = await get(usernameSchema)
-    console.log(username)
-    
+const prompt = require('prompt')
+const { JSDOM }= require('jsdom')
+const { ChatManager, TokenProvider } = require('@pusher/chatkit')
+const readline = require('readline')
+
+const makeChatkitNodeCompatible = () => {
+    const { window } = new JSDOM()
+    global.window = window
+    global.navigator = {}
+}
+
+const createUser = async username => {
+    makeChatkitNodeCompatible()
+    try{
+        
+        await axios.post('http://localhost:3001/users', {
+            username
+        })
+
+    } catch (error) {
+        console.error(error)
     }
-} 
-catch (error) {
-    console.error(error)
-    process.exit(1)
+}
+
+
+const main = async () => {
+
+    try {
+   
+        prompt.start()
+        prompt.message = " "
+         const get = util.promisify(prompt.get)
+     
+         const usernameSchema = [
+             {
+                 description: 'Enter your username' ,
+                 name: 'username' , 
+                 require: true
+             }
+         ]
+         const {username} = await get(usernameSchema)
+         await createUser(username)
+
+         const chatManager = new ChatManager({
+            instanceLocator: 'v1:us1:f40ef635-3256-40f0-9124-7a554253bf3d',
+            userId: username,
+            tokenProvider: new TokenProvider({ url: 'http://localhost:3001/authenticate' })
+          })
+          const currentUser = await chatManager.connect()
+          const joinableRooms = await currentUser.getJoinableRooms();
+          const availableRooms = [...currentUser.rooms, ...joinableRooms];
+
+          availableRooms.forEach((room, index) => {
+            console.log(`${index} - ${room.name}`);
+          });
+
+          const roomSchema = [{
+            description: "Select a room",
+            name: "chosenRoom",
+            require: true
+           
+          }];
+
+          const { chosenRoom } = await get(roomSchema)
+          const room = availableRooms[chosenRoom]
+
+          await currentUser.subscribeToRoom({
+            roomId: room.id,
+            hooks: {
+              onNewMessage: message => {
+                const { senderId, text } = message;
+                if (senderId === username) return;
+                console.log(`${senderId}: ${text}`);
+              }
+            },
+            messageLimit: 0
+          });
+
+          const input = readline.createInterface({
+              input: process.stdin
+          })
+
+          input.on('line', async text => {
+              await currentUser.sendMessage({
+                  roomId: room.id,
+                  text
+              })
+          })
+        } 
+       
+    catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+
+   
 }
 
 
